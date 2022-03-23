16aca8caf869eb8dcffaf3c9e63872417fcc9f5f
junwvwv <wvwv0612@gmail.com>
2022-03-23 21:16:19 +0900

    java 예제

---
+++ src/ControlFlow/If03.java
@@ -0,0 +1,24 @@
+package ControlFlow;
+
+import java.util.Random;
+
+public class If03 {
+
+	public static void main(String[] args) {
+		
+		//주사위 굴리기
+		
+		Random random = new Random();
+		int[] num = new int[2];
+		
+		num[0] = random.nextInt(6) + 1;		
+		num[1] = random.nextInt(6) + 1;	
+		
+		if((num[0] + num[1]) > 6) {
+			System.out.println("당첨입니다.");
+		}else {
+			System.out.println("꽝입니다.");
+		}
+		
+	}
+}
---
+++ src/ControlFlow/Switch01.java
@@ -0,0 +1,38 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class Switch01 {
+
+	public static void main(String[] args) {
+
+		//음료수 자판기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int price = 0;
+		System.out.print("원하시는 음료를 고르십시오: ");
+		String order = sc.nextLine();
+		
+		switch(order) {
+		case "레쓰비":
+			price = 600;
+			break;
+		case "코카콜라":
+			price = 1200;
+			break;
+		case "칠성사이다":
+			price = 1000;
+			break;
+		case "파워에이드":
+			price = 900;
+			break;
+		default:
+			System.out.println("선택하신 커피가 메뉴에 없습니다.");
+		}
+		if(price != 0) {
+			System.out.println(order + "는 " + price + "원 입니다.");
+		}
+	}
+
+}
