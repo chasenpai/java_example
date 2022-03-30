5e658c96af73b7ca753fd9f3bf42f5d8e962c36c
junwvwv <wvwv0612@gmail.com>
2022-03-30 20:17:19 +0900

    java 예제

---
+++ src/example/array/MultiArray01.java
@@ -0,0 +1,35 @@
+package example.array;
+
+import java.util.Scanner;
+
+public class MultiArray01 {
+
+	public static void main(String[] args) {
+
+		//2차원 배열로 학점 구하기
+		double score[][] = new double[4][2];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double sum = 0;
+		
+		for(int i = 0; i < score.length; i++) {
+			for(int j = 0; j < score[i].length; j++) {
+				System.out.println((i+1) + "학년 " + (j+1) + "학기 " + "학점을 입력하시오: ");
+				score[i][j] = sc.nextDouble(); //입력받은 값이 배열의 i행 j번열 인덱스에 담긴다
+				sum += score[i][j];
+				
+			}
+			
+		}
+		System.out.println();
+		for(int i = 0; i < score.length; i++) {
+			for(int j = 0; j < score[i].length; j++) {
+				System.out.println((i+1) + "학년 " + (j+1) + "학기 "+ score[i][j]);
+			}
+		}
+		System.out.println();
+		System.out.println("평점: " + String.format("%.2f", sum/(score.length * score[0].length)));
+	}
+
+}
